c2d492ae8861a821fb054468180e58f2d69a8818
rere1616 <124903220+rere1616@users.noreply.github.com>
2023-02-18 07:46:50 +0900

    Update admin.js

--- commands/admin.js
+++ commands/admin.js
@@ -1,4 +1,9 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const moment = require('moment');
+require('moment-timezone');
+moment.tz.setDefault("Asia/Seoul");
+
+var timestamp = moment().format('HH:mm:ss');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -7,6 +12,6 @@ module.exports = {
   async execute(interaction) {
     const guild = await client.guilds.cache.get(interaction.guild_id);
     const user = await client.users.cache.get(interaction.member.user.id);
-    user.send(`${interaction.user}, invalid Access.`).catch(console.error);
+    user.send(`${interaction.user}, invalid Access.`).then(console.log('<<admin.js>> ' + timestamp + ` Invalid attempting to access by ${interaction.user}`)).catch(console.error);
   },
 };
