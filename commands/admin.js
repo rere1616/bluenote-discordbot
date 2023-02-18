55670f3b234ec59007277882fe6ef0e3e7faec6d
rere1616 <124903220+rere1616@users.noreply.github.com>
2023-02-19 08:23:01 +0900

    Update admin.js

--- commands/admin.js
+++ commands/admin.js
@@ -11,15 +11,10 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("admin")
     .setDescription("Access to the bot as authorized admin."),
-  async execute(interaction) {
+    async execute(interaction) {
+  		await interaction.reply('Pong!');
+  	},
 //    let guild = client.guilds.cache.get(interaction.guild_id);
-console.log('1: ' + interaction.user.id).catch(console.error);
-console.log('3: ' + `${interaction.user.id}`).catch(console.error);
-console.log('5: ' + `${interaction.user.username}`).catch(console.error);
-await interaction.reply(
-  `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
-);
 //    let user = client.users.cache.get(interaction.user.id);
 //    await user.send(`${interaction.user.username}, invalid Access.`).then(console.log('<<admin.js>> ' + timestamp + ` Invalid attempting to access by ${interaction.user.username}`)).catch(console.error);
-  },
 };
