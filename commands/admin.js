9badf9d2a50da6ebeee57c60311f237c6bf47cc0
rere1616 <124903220+rere1616@users.noreply.github.com>
2023-02-19 07:48:42 +0900

    Update admin.js

--- commands/admin.js
+++ commands/admin.js
@@ -12,8 +12,8 @@ module.exports = {
     .setName("admin")
     .setDescription("Access to the bot as authorized admin."),
   async execute(interaction) {
-    let guild = client.guilds.cache.get(interaction.guild_id);
-    let user = client.users.cache.get(interaction.member.user.id);
-    await user.send(`${interaction.user.username}, invalid Access.`).then(console.log('<<admin.js>> ' + timestamp + ` Invalid attempting to access by ${interaction.user}`)).catch(console.error);
+//    let guild = client.guilds.cache.get(interaction.guild_id);
+    let user = client.users.cache.get(interaction.user.id);
+    await user.send(`${interaction.user.username}, invalid Access.`).then(console.log('<<admin.js>> ' + timestamp + ` Invalid attempting to access by ${interaction.user.username}`)).catch(console.error);
   },
 };
