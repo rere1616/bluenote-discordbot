a6e3b3b73c1768fb91c89bfc52777f0fd5bc563d
rere1616 <124903220+rere1616@users.noreply.github.com>
2023-02-19 06:21:40 +0900

    0.0.3

--- commands/admin.js
+++ commands/admin.js
@@ -1,17 +1,17 @@
 const client = require("../index.js");
-const { SlashCommandBuilder } = require("@discordjs/builders");
+const { SlashCommandBuilder } = require('discord.js');
 
 const moment = require('moment');
 require('moment-timezone');
 moment.tz.setDefault("Asia/Seoul");
 
-var timestamp = moment().format('HH:mm:ss');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("admin")
-    .setDescription("Access to the bot as authorized admin."),
+    .setDescription("Access to this bot as authorized admin."),
   async execute(interaction) {
+    var timestamp = moment().format('HH:mm:ss');
     const guild = await client.guilds.cache.get(interaction.guild_id);
     const user = await client.users.cache.get(interaction.member.user.id);
     user.send(`${interaction.user}, invalid Access.`).then(console.log('<<admin.js>> ' + timestamp + ` Invalid attempting to access by ${interaction.user}`)).catch(console.error);
