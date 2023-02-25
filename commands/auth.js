f58ce5088fbcbff980a4afb28b3c0738fe14d877
rere1616 <124903220+rere1616@users.noreply.github.com>
2023-02-25 14:02:18 +0900

    Update auth.js

--- commands/auth.js
+++ commands/auth.js
@@ -5,15 +5,17 @@ const moment = require('moment');
 require('moment-timezone');
 moment.tz.setDefault("Asia/Seoul");
 
-async function compareid(v) {
-  const fs = await require('fs');
-  const readline = await require('readline');
-  const rl = await readline.createInterface({
+function compareid(v) {
+  const fs = require('fs');
+  const readline = require('readline');
+  const rl = readline.createInterface({
        input: fs.createReadStream('./tmp/adminid.txt')
   });
-  rl.on('line', function (line) {
-    if (line === v) { return v; }
-    else { return; }
+  return new Promise((resolve, reject) => {
+    rl.on('line', function (line) {
+      if (line === v) { resolve(line); }
+      else { return; }
+    })
   })
 };
 
@@ -35,12 +37,19 @@ client.on(Events.InteractionCreate, async interaction => {
 
 	if (interaction.commandName === 'auth') {
     const timestamp = await moment().format('HH:mm:ss');
-    const userid = await `'${interaction.user.tag}'`
-    compareid(userid).then(console.log,console.err)
-//    else if () {
-//      await interaction.reply({ content: `${interaction.user.tag}, Invalid Access.`, ephemeral: true });
-//      console.log(`\n<<auth.js>> ` + timestamp + ` An invalid access attempt was made by ${interaction.user.tag}\n`)
-//    }
+    const userid = await `'${interaction.user.tag}'`;
 
+    let getverified = await compareid(userid);
+    if (!getverified) {
+      await interaction.reply({ content: `${interaction.user.tag}, Invalid Access.`, ephemeral: true });
+      console.log(`\n<<auth.js>> ` + timestamp + ` An invalid access attempt was made by ${interaction.user.tag}\n`)
+//      client.user.fetch('343282328858132484').send(`An invalid access attempt was made by ${interaction.user.tag}`)
+    }
+    else if(getverified) {
+      await interaction.reply({ content: `${interaction.user.tag}, Logged on.`, ephemeral: true });
+      console.log(`\n<<auth.js>> ` + timestamp + ` ${interaction.user.id} ${interaction.user.tag} has just logged on.\n`)
+//      client.user.fetch('343282328858132484').send(`An invalid access attempt was made by ${interaction.user.tag}`)
+    }
+    else console.err
 	}
 });
