0c934c96dfba00330180e09ca34c7c1caf3d1630
rere1616 <124903220+rere1616@users.noreply.github.com>
2023-02-25 11:06:36 +0900

    Update auth.js

--- commands/auth.js
+++ commands/auth.js
@@ -6,9 +6,9 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("auth")
     .setDescription("Access to the bot as authorized admin."),
-    async execute(interaction) {
-      await interaction.reply({ content: `Invalid Access.`, ephemeral: true });
-  	},
+//    async execute(interaction) {
+//      await interaction.reply({ content: `Invalid Access.`, ephemeral: true });
+//  	},
 //    let guild = client.guilds.cache.get(interaction.guild_id);
 //    let user = client.users.cache.get(interaction.user.id);
 //    await user.send(`${interaction.user.username}, invalid Access.`).then(console.log('<<admin.js>> ' + timestamp + ` Invalid attempting to access by ${interaction.user.username}`)).catch(console.error);
