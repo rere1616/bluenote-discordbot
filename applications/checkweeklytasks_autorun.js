024664da4a02ed6c39287c1c7dc4d1596d2c81de
rere1616 <124903220+rere1616@users.noreply.github.com>
2023-03-07 02:07:04 +0900

    Update checkweeklytasks_autorun.js

--- applications/checkweeklytasks_autorun.js
+++ applications/checkweeklytasks_autorun.js
@@ -123,7 +123,9 @@ function ontime_check(fn, t) {
 }
 
 ///////////////  채널 출력 메세지 생성
-async function loadmsn(t, n) {
+var mission;
+
+async function loadms(t, n) {
   const readline = require('readline');
   const rl = readline.createInterface({
        input: fs.createReadStream(t)
@@ -169,8 +171,6 @@ async function Weekly_checkdate() {
 //  const rl = readline.createInterface({
 //    input: fs.createReadStream('tmp/taskslist.txt')
 //  });
-  var mission;
-
   var taskcount =  fs.readFileSync('tmp/WeeklyTasksCount.txt', {encoding:'utf8', flag:'r'});
 //  WeeklyTasksCount.txt 파일 읽기
 
@@ -179,10 +179,10 @@ async function Weekly_checkdate() {
   if (daynow == 2){
 
     if((timenow >= 000000) && (timenow < 221000)) {
-      let datenxt = moment().add(1, 'day')
-      let mission = loadmsn('./tmp/taskslist.txt', './tmp/WeeklyTasksCount.txt');
+      let datenxt = await moment().add(1, 'day')
+      let mission = await loadms('./tmp/taskslist.txt', './tmp/WeeklyTasksCount.txt');
 //let mission = `큐브, 위험해역, 고고학`
-      let weekly_chanmsg = createmsg(datenxt.format('YYYY-MM-DD'), week[(daynow + 1)], mission);
+      let weekly_chanmsg = await createmsg(datenxt.format('YYYY-MM-DD'), week[(daynow + 1)], mission);
       const channel = await client.channels.fetch(chanID1);
       channel.send({ embeds: [weekly_chanmsg] })
       .then(console.log('[' + timestamp + '] ' + path.basename(__filename) + '>> Weekly_checkdate>> Outputting messages...'))
@@ -212,7 +212,7 @@ async function Weekly_checkdate() {
   else if (daynow == 3) {
 
     if((timenow >= 100000) && (timenow < 101000)) {
-//      let mission = await loadmsn('./tmp/taskslist.txt', './tmp/WeeklyTasksCount.txt');
+//      let mission = await loadms('./tmp/taskslist.txt', './tmp/WeeklyTasksCount.txt');
 let mission = `큐브, 위험해역, 고고학`
       let weekly_chanmsg = await createmsg(datenow, week[daynow], mission);
       const channel = await client.channels.fetch(chanID1);
