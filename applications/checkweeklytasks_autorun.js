c0f5b6f6ddba62661e3f1e2ae98282a4c9c59939
rere1616 <124903220+rere1616@users.noreply.github.com>
2023-03-22 08:46:18 +0900

    Update checkweeklytasks_autorun.js

--- applications/checkweeklytasks_autorun.js
+++ applications/checkweeklytasks_autorun.js
@@ -190,14 +190,16 @@ const rl = readline.createInterface({
       let taskcount = await fs.readFileSync(`./tmp/WeeklyTasksCount.txt`, {encoding:'utf8', flag:'r'})
       let s = line.split(':');
       var mission = s[taskcount];
-    })
 
-//      let mission = await loadmsn(`./tmp/taskslist.txt`, `./tmp/WeeklyTasksCount.txt`);
-      let weekly_chanmsg = await createmsg(datenxt.format('YYYY-MM-DD'), week[(daynow + 1)], mission);
-      const channel = await client.channels.fetch(chanID1);
-      channel.send({ embeds: [weekly_chanmsg] })
-      .then(console.log('[' + timestamp + '] ' + path.basename(__filename) + '>> Weekly_checkdate>> Outputting messages...'))
-      .catch(console.error);
+
+      //      let mission = await loadmsn(`./tmp/taskslist.txt`, `./tmp/WeeklyTasksCount.txt`);
+            let weekly_chanmsg = await createmsg(datenxt.format('YYYY-MM-DD'), week[(daynow + 1)], mission);
+            const channel = await client.channels.fetch(chanID1);
+            channel.send({ embeds: [weekly_chanmsg] })
+            .then(console.log('[' + timestamp + '] ' + path.basename(__filename) + '>> Weekly_checkdate>> Outputting messages...'))
+            .catch(console.error);
+
+    })
 
 
 /*      rl.on('line', function (line) {
@@ -228,14 +230,15 @@ const rl = readline.createInterface({
       let taskcount = await fs.readFileSync(`./tmp/WeeklyTasksCount.txt`, {encoding:'utf8', flag:'r'})
       let s = line.split(':');
       var mission = s[taskcount];
-    })
 
-//      let mission = await loadmsn(`./tmp/taskslist.txt`, `./tmp/WeeklyTasksCount.txt`);
-      let weekly_chanmsg = await createmsg(datenow, week[daynow], mission);
-      const channel = client.channels.fetch(chanID1);
-      channel.send({ embeds: [weekly_chanmsg] })
-      .then(console.log('[' + timestamp + '] ' + path.basename(__filename) + '>> Weekly_checkdate>> Outputting messages...'))
-      .catch(console.error);
+      //      let mission = await loadmsn(`./tmp/taskslist.txt`, `./tmp/WeeklyTasksCount.txt`);
+            let weekly_chanmsg = await createmsg(datenow, week[daynow], mission);
+            const channel = client.channels.fetch(chanID1);
+            channel.send({ embeds: [weekly_chanmsg] })
+            .then(console.log('[' + timestamp + '] ' + path.basename(__filename) + '>> Weekly_checkdate>> Outputting messages...'))
+            .catch(console.error);
+
+    })
 
 
 /*      rl.on('line', function (line) {
