8ae62c103514fe2a77ea322b5c34ba30b6ed1543
rere1616 <124903220+rere1616@users.noreply.github.com>
2023-03-22 07:56:01 +0900

    Update checkweeklytasks_autorun.js

--- applications/checkweeklytasks_autorun.js
+++ applications/checkweeklytasks_autorun.js
@@ -212,6 +212,7 @@ async function Weekly_checkdate() {
 
     if ((timenow >= 000000) && (timenow < 101000)) {
       let mission = await loadmsn(`./tmp/taskslist.txt`, `./tmp/WeeklyTasksCount.txt`);
+      console.log(mission)
       let weekly_chanmsg = await createmsg(datenow, week[daynow], mission);
       const channel = client.channels.fetch(chanID1);
       channel.send({ embeds: [weekly_chanmsg] })
