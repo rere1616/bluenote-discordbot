5510118dce750cf47591c7d1c3ca801d5c33f9eb
rere1616 <124903220+rere1616@users.noreply.github.com>
2023-03-28 01:38:07 +0900

    Update checkweeklytasks_autorun.js

--- applications/checkweeklytasks_autorun.js
+++ applications/checkweeklytasks_autorun.js
@@ -174,7 +174,7 @@ async function Weekly_checkdate() {
 //  });
 //  var mission;
 
-  var taskcount =  await fs.readFileSync('tmp/WeeklyTasksCount.txt', {encoding:'utf8', flag:'r'});
+  var tcount = await fs.readFileSync('tmp/WeeklyTasksCount.txt', {encoding:'utf8', flag:'r'});
 //  WeeklyTasksCount.txt 파일 읽기
 
 
@@ -185,10 +185,9 @@ async function Weekly_checkdate() {
       let datenxt = moment().add(1, 'day')
 //      let mission = await loadmsn('./tmp/taskslist.txt', './tmp/WeeklyTasksCount.txt');
 
-      let tcount = await fs.readFileSync('./tmp/WeeklyTasksCount.txt', {encoding:'utf8', flag:'r'})
       let mission = await tlist.split(':')[tcount];
 
-      let weekly_chanmsg = await createmsg(datenxt.format('YYYY-MM-DD'), week[(daynow + 1)], mission);
+      let weekly_chanmsg = createmsg(datenxt.format('YYYY-MM-DD'), week[(daynow + 1)], mission);
       const channel = await client.channels.fetch(chanID1);
       channel.send({ embeds: [weekly_chanmsg] })
       .then(console.log('[' + timestamp + '] ' + path.basename(__filename) + '>> Weekly_checkdate>> Outputting messages...'))
